55d5d675f7cdb566b3bab7b380ba12f2081b7c90
nesquikm <drapolyuk@gmail.com>
2024-05-01 12:12:29 +0400

    feat: add clean script for flutter app

---
+++ ohclays_flutter/scripts/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove build files generated code
+find . -type f -name "*_generated.*" -delete
+find . -type f -name "*.freezed.dart" -delete
+find . -type f -name "*.g.dart" -delete
+find . -type f -name "*.gen.dart" -delete
+find . -type f -name "*.reflectable.dart" -delete
+find . -type f -name "*.module.dart" -delete
+
